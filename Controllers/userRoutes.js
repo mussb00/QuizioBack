d6678ed937bf615cdd651652b58456329b2ee1ba
mussb00 <berhanem2000@gmail.com>
2021-10-28 18:34:34 +0100

    top 5 user scores now returned in order

--- Controllers/userRoutes.js
+++ Controllers/userRoutes.js
@@ -12,7 +12,7 @@ router.use(express.json())
 router.get('/leaderboard', async (req, res) => {
     try {
         await mongoose.connect(process.env.CONNECTION_URL)
-        const usersWithScores = await User.find({total_scores: {$ne : null}})
+        const usersWithScores = await (await User.find({total_scores: {$ne : null}}).sort({total_scores:-1})).slice(0,5)
         res.send(usersWithScores)
         // orders score from highest to lowest
         // const orderedList = allUsers.map(user => user.total_scores).sort((a, b) => b - a)
