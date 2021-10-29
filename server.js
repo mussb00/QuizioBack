d07e67d95f73c7bceb7ca28565a5a575db3660f4
Quinn Lanphere <qlanphere@yahoo.com>
2021-10-29 10:15:40 +0100

    verify

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ io.on('connection', socket => {
 app.use('/user', userRoutes)
 app.use('/auth', authRoutes)
 app.get('/', (req, res)=>{res.send('Welcome to our Quiz API!')})
+app.use(verifyToken)
 
 
 //    moved this to index.js
