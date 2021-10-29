01b3d8d847bff908f2fcbc91dd6a61b6d5ee9850
mussb00 <berhanem2000@gmail.com>
2021-10-29 01:07:20 +0100

    another one

--- Controllers/userRoutes.js
+++ Controllers/userRoutes.js
@@ -12,7 +12,7 @@ router.use(express.json())
 
 router.get('/leaderboard', async (req, res) => {
     try {
-        await mongoose.connect(process.env.CONNECTION_URL, {
+        await mongoose.connect('mongodb+srv://mussie:quizioback@cluster0.hhnk4.mongodb.net/quizioback', {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useFindAndModify: false
