b384fe4a8b175b1e10e8fde7e92352b070d3f364
mussb00 <berhanem2000@gmail.com>
2021-10-29 01:10:42 +0100

    final one

--- Controllers/userRoutes.js
+++ Controllers/userRoutes.js
@@ -12,12 +12,8 @@ router.use(express.json())
 
 router.get('/leaderboard', async (req, res) => {
     try {
-        await mongoose.connect('mongodb+srv://mussie:quizioback@cluster0.hhnk4.mongodb.net/quizioback', {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useFindAndModify: false
-        })
-        const usersWithScores = await User.find({ total_scores: { $ne: null } }).sort({ total_scores: -1 }).slice(0, 5)
+        await mongoose.connect(process.env.CONNECTION_URL)
+        const usersWithScores = await  User.find({total_scores: {$ne : null}}).sort({total_scores:-1}).limit(5)
         res.send(usersWithScores)
         // orders score from highest to lowest
         // const orderedList = allUsers.map(user => user.total_scores).sort((a, b) => b - a)
@@ -33,15 +29,15 @@ router.get('/:emails', async (req, res) => {
         await mongoose.connect(process.env.CONNECTION_URL)
         const emailString = req.params.emails
         const emailArray = emailString.split('*')
-
-        const usersInRoom = await Promise.all(emailArray.map(email => User.findOne({ email: email })))
+        
+        const usersInRoom = await Promise.all(emailArray.map(email => User.findOne({email: email})))
         res.send(usersInRoom)
     } catch (err) {
         res.status(404).send(err)
     }
 })
 
-router.patch('/:email', verifyToken, async (req, res) => {
+router.patch('/:email',verifyToken, async (req, res) => {
     try {
         await mongoose.connect(process.env.CONNECTION_URL)
 
@@ -52,8 +48,8 @@ router.patch('/:email', verifyToken, async (req, res) => {
             }
         })
 
-        user = await User.updateOne({ email: req.params.email },
-            { last_score: req.body.game_score })
+        user = await User.updateOne({email:req.params.email},
+            {last_score: req.body.game_score})
 
         res.send(user)
         disconnect()
