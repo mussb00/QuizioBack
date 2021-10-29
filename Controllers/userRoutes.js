66b35c7b664da9e5e2ff0ecb8f9269c3e0b8ddae
mussb00 <berhanem2000@gmail.com>
2021-10-29 01:05:45 +0100

    another one

--- Controllers/userRoutes.js
+++ Controllers/userRoutes.js
@@ -12,8 +12,12 @@ router.use(express.json())
 
 router.get('/leaderboard', async (req, res) => {
     try {
-        await mongoose.connect(process.env.CONNECTION_URL)
-        const usersWithScores = await  User.find({total_scores: {$ne : null}}).sort({total_scores:-1}).slice(0,5)
+        await mongoose.connect(process.env.CONNECTION_URL, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        const usersWithScores = await User.find({ total_scores: { $ne: null } }).sort({ total_scores: -1 }).slice(0, 5)
         res.send(usersWithScores)
         // orders score from highest to lowest
         // const orderedList = allUsers.map(user => user.total_scores).sort((a, b) => b - a)
@@ -29,15 +33,15 @@ router.get('/:emails', async (req, res) => {
         await mongoose.connect(process.env.CONNECTION_URL)
         const emailString = req.params.emails
         const emailArray = emailString.split('*')
-        
-        const usersInRoom = await Promise.all(emailArray.map(email => User.findOne({email: email})))
+
+        const usersInRoom = await Promise.all(emailArray.map(email => User.findOne({ email: email })))
         res.send(usersInRoom)
     } catch (err) {
         res.status(404).send(err)
     }
 })
 
-router.patch('/:email',verifyToken, async (req, res) => {
+router.patch('/:email', verifyToken, async (req, res) => {
     try {
         await mongoose.connect(process.env.CONNECTION_URL)
 
@@ -48,8 +52,8 @@ router.patch('/:email',verifyToken, async (req, res) => {
             }
         })
 
-        user = await User.updateOne({email:req.params.email},
-            {last_score: req.body.game_score})
+        user = await User.updateOne({ email: req.params.email },
+            { last_score: req.body.game_score })
 
         res.send(user)
         disconnect()
